ad7da064e036e02e11594e84c7a957784df5c0ba
amir guidoum <amirguidoum49@gmail.com>
2025-07-23 05:38:02 -0400

    commit 1

--- middleware/validations/userValidation.js
+++ middleware/validations/userValidation.js
@@ -3,7 +3,7 @@ const { celebrate, Joi, Segments } = require("celebrate");
 const updateUserValidation = celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    email: Joi.string().email().required(),
+    avatar: Joi.string().uri().required(),
   }),
 });
 
