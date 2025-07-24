13e2e899d8f5c57c5696d7fd8f2111e52bb6f50d
amir guidoum <amirguidoum49@gmail.com>
2025-07-24 09:51:26 -0400

    commit7

--- middleware/errorHandle.js
+++ middleware/errorHandle.js
@@ -23,10 +23,19 @@ class ServerError extends AppError {
     super(message, 500);
   }
 }
+function errorHandler(err, req, res, next) {
+  console.error(err);
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({ error: message });
+}
 
 module.exports = {
   BadRequestError,
   NotFoundError,
   ForbiddenError,
   ServerError,
+  errorHandler,
 };
